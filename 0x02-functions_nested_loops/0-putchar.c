35776657c8e87651d3a0474c883f34199b419255
Obrabigfish <obrabigfish1@gmail.com>
2023-10-11 01:19:08 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
+#include <main.h>
 /**
- * main - Entry
+ * main - Prints putchar
  * Return: 0 (Success)
  */
 int main(void)
 {
-	char a;
-
-	a = _putchar;
-	putchar('a');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
