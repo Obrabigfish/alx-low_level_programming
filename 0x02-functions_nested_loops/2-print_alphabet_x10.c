d7e69b4803475ad0a0f8713b56cd404a6a05c42a
Obrabigfish <obrabigfish1@gmail.com>
2023-10-11 03:39:03 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
 #include "main.h"
-
+/**
+ * print_alphabet_x10 - Prints in lowercase ten times
+ */
 void print_alphabet_x10(void)
 {
-	char b = 'a';
-
-	int i = 0;
-	
-	while (i < 10)
-	{
-		for (b = 'a'; b <= 'z'; b++)
-	{
-		_putchar(b);
-	}
-		_putchar('\n');
-		i++;
-	}
+char b = 'a';
+int i = 0;
+while (i < 10)
+{
+for (b = 'a'; b <= 'z'; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+i++;
+}
 }
