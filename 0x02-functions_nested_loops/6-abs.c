aec73b28f92529e976c25c1aaa84839b2e64a059
Obrabigfish <obrabigfish1@gmail.com>
2023-10-12 04:17:47 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _abs - Checks for absolute value
+ * int - Data type integer
+ * Return: 0 
+ */
+int _abs(int x)
+{
+	if (x < '0')
+	{
+		return (-x);
+	}
+	else
+	{
+	return (x);
+	}
+}
