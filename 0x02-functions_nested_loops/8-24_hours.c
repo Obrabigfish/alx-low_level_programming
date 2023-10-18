a94d8a64cce5a24b93db1684207483a6dda6f5d3
Obrabigfish <obrabigfish1@gmail.com>
2023-10-18 02:50:43 -0700

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,17 +10,16 @@ void jack_bauer(void)
 	int a;
 	int b;
 
-    for (a = 0; a < 24; a++)
+	for (a = 0; a < 24; a++)
 	{
-        for (b = 0; b < 60; b++)
+		for (b = 0; b < 60; b++)
 		{
-            putchar((a / 10) + '0');
-            putchar((a % 10) + '0');
-            putchar(':');
-            putchar((b / 10) + '0');
-            putchar((b % 10) + '0');
-            putchar('\n');
-        }
-    }
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(':');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			putchar('\n');
+		}
+	}
 }
-
