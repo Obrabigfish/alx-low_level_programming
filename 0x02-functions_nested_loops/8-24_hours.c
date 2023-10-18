3e2774c2385aa4036edfdf9fa569eaf15ce1d26d
Obrabigfish <obrabigfish1@gmail.com>
2023-10-18 02:45:48 -0700

    8-24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * jack_bauer - function prototype
+ * @a - gives hour
+ * @b - gives minutes
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+    for (a = 0; a < 24; a++)
+	{
+        for (b = 0; b < 60; b++)
+		{
+            putchar((a / 10) + '0');
+            putchar((a % 10) + '0');
+            putchar(':');
+            putchar((b / 10) + '0');
+            putchar((b % 10) + '0');
+            putchar('\n');
+        }
+    }
+}
+
