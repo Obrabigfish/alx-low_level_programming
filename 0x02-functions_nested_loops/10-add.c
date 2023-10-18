b83425ebefeb885a6230fe12911901387b772453
Obrabigfish <obrabigfish1@gmail.com>
2023-10-18 05:45:16 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function namie
+ * a - first number to be added
+ * b - second number to be added
+ * @result - takes in the values
+ * Return: reult
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
