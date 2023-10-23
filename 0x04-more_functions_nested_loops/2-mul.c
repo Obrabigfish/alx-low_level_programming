6ff0c6b45e383567833ef5da09b8da94153b47bb
Obrabigfish <obrabigfish1@gmail.com>
2023-10-23 13:26:07 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two values
+ * @a: integer
+ * @b: integer
+ * Return: multiplied variables
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	printf("%d", c);
+	return (c);
+}
