6c362dd5fa659e30cb013f7584626a87f59331d3
Obrabigfish <obrabigfish1@gmail.com>
2023-10-23 14:30:34 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - searches for digits
+ */
+void print_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
