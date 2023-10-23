88c17e96928a94d67e6b16858bdc443a671f126f
Obrabigfish <obrabigfish1@gmail.com>
2023-10-23 13:10:53 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  _isupper - checks for uppercase
+ * @c: variable to be checked
+ * Return: 1 if uppercase else 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
