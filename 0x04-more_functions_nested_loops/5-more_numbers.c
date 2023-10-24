20bf8a53cc2689ab9ce587d82cf18da399c0bef5
Obrabigfish <obrabigfish1@gmail.com>
2023-10-24 12:27:37 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers
+ */
+void more_numbers(void)
+{
+	int c;
+	int b;
+	
+	for (b = 0; b < 10; b++)
+	{
+	for (c = 0; c <= 14; c++)
+	{
+		if (c > 9)
+	{
+		putchar(c / 10 + '0');
+	}
+	putchar(c % 10 + '0');
+	}
+	putchar('\n');
+	}
+}
