4bb6f8337ab2ddba88c5f4b35bacf0747910c47e
obrabigfish <obrabigfish1@gmail.com>
2025-03-12 17:00:27 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main: Entry point of program
+ *
+ * Description: This prints using write
+ *
+ * Return 0 (Success)
+ */
+int main()
+{
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fprintf(stderr, "%s\n", n);
+	return (1);
+}
