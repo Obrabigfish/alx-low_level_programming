37c03a592b18e17611baed826f93d632b8a95758
obrabigfish <obrabigfish1@gmail.com>
2025-03-19 12:38:29 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 1; b <= 9; b++)
+		{
+			if (a == b)
+				continue;
+			else if (a > b)
+				continue;
+			else
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+	}
+	return (0);
+}
