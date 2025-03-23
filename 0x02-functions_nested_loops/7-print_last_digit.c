5eb93d74d19b7834f3b5920951770ce74c12012f
obrabigfish <obrabigfish1@gmail.com>
2025-03-23 10:26:31 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,17 @@
 #include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @a: number to be checked
+ * Return: Always 0
+ */
 
 int print_last_digit(int a)
 {
 	int b;
+
 	b = a % 10;
+	if (a  < 0)
+		b = -b;
 	_putchar(b + '0');
 	return (0);
 }
